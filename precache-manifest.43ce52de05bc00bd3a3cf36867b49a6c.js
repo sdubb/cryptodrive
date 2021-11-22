37623165ee26509ffc870455fc7d5a7cc92f9e61
SHUBHAM DUBEY <shuubhamdubey3131@gmail.com>
2021-11-22 15:49:22 +0000

    Updates

---
+++ precache-manifest.43ce52de05bc00bd3a3cf36867b49a6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20a9aceb3e8f4980081884623360aa9",
+    "url": "/cryptodrive/index.html"
+  },
+  {
+    "revision": "f4c92025d59ff2722aa3",
+    "url": "/cryptodrive/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "563669d38a264b569625",
+    "url": "/cryptodrive/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "f4c92025d59ff2722aa3",
+    "url": "/cryptodrive/static/js/2.67162a96.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/cryptodrive/static/js/2.67162a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "563669d38a264b569625",
+    "url": "/cryptodrive/static/js/main.d8ba1d59.chunk.js"
+  },
+  {
+    "revision": "515e79a3ea24588b009f",
+    "url": "/cryptodrive/static/js/runtime-main.3bcc9c4c.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/cryptodrive/static/media/box.df8b84d5.png"
+  }
+]);
